2371b6674e7bec9e537e425784b824677cdd44af
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-10 13:16:13 -0300

    Requisito [5]

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const input = document.getElementById('carta-texto'); // [3]
 const createLetterButton = document.getElementById('criar-carta'); // [3]
 const generatedLetter = document.getElementById('carta-gerada'); // [3]
+
 function limpaSpan() {
   generatedLetter.innerHTML = '';
 }
 function createLetter() {
-  const values = input.value.split(/\s+/);
+  let values = input.value.split(/\s+/);
+  if (input.value === '' || input.value[0] === ' ') {
+    values = 'Por favor, digite o conteúdo da carta.';
+  }
   console.log(values, input.value);
   limpaSpan();
   for (let index = 0; index < values.length; index += 1) {
