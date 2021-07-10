9c8517e91e72f612264a95391ee7c875e53dcea9
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-10 18:48:18 -0300

    Requisito [16] [17]

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const input = document.getElementById('carta-texto'); // [3]
 const createLetterButton = document.getElementById('criar-carta'); // [3]
 const generatedLetter = document.getElementById('carta-gerada'); // [3]
+const estilo = ['newspaper', 'magazine1', 'magazine2'];
+const tamanho = ['medium', 'big', 'reallybig'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
 
+// [16]
+function sorteia(numero) {
+  return Math.floor(Math.random() * numero);
+}
+
+function listaClasses() {
+  const A = sorteia(3);
+  const B = sorteia(3);
+  const C = sorteia(2);
+  const D = sorteia(2);
+
+  const classes = [estilo[A], tamanho[B], rotacao[C], inclinacao[D]];
+  // classes.s;
+  return classes;
+}
+
+function adicionaClasse(elemento) {
+  const classes = listaClasses();
+  const primeiraClasse = sorteia(4);
+  let segundaClasse = sorteia(4);
+  while (segundaClasse === primeiraClasse) {
+    segundaClasse = sorteia(4);
+  }
+  elemento.classList.add(classes[primeiraClasse]);
+  elemento.classList.add(classes[segundaClasse]);
+  console.log(listaClasses());
+}
+
+// [3]
 function limpaSpan() {
   generatedLetter.innerHTML = '';
 }
@@ -10,12 +43,12 @@ function createLetter() {
   if (input.value === '' || input.value[0] === ' ') {
     values = 'Por favor, digite o conteúdo da carta.';
   }
-  console.log(values, input.value);
   limpaSpan();
   for (let index = 0; index < values.length; index += 1) {
     if (values[index] !== '') {
       const span = document.createElement('span');
       span.innerText = values[index];
+      adicionaClasse(span);
       generatedLetter.appendChild(span);
     }
   }
