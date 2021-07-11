31bd3cc4fe9bc9177a1bbe7b4b2c1782cad3a947
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-11 13:39:51 -0300

    Requisito número 1

---
+++ script.js
@@ -0,0 +1,5 @@
+const headerSelect = document.querySelector('#header');
+
+const inputCreate = document.createElement('input');
+inputCreate.setAttribute('id', 'carta-texto');
+headerSelect.appendChild(inputCreate);
