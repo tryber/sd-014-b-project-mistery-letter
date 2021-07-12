5ad4708b30d6bdf4379a4ff8771d4021cee634e4
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-07-12 16:51:10 -0300

    Requisito número 16

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const letterInput = document.getElementById('carta-texto');
 const generatedLetter = document.getElementById('carta-gerada');
 const letterGeneratorBtn = document.getElementById('criar-carta');
 let typedText = '';
+const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
+const sizeGroup = ['medium', 'big', 'reallybig'];
+const rotationGroup = ['rotateleft', 'rotateright'];
+const inclinationGroup = ['skewleft', 'skewright'];
 
 function getInput(event) {
   if (event.target.value === '' || event.target.value === ' ') {
@@ -18,6 +22,25 @@ function createSpan() {
     const createdSpan = document.createElement('span');
     createdSpan.innerText = stringArr[index];
     generatedLetter.appendChild(createdSpan);
+  }
+  decorateStyleSize();
+  decorateRotationInclination();
+}
+
+function decorateStyleSize() {
+  for (let index = 0; index < generatedLetter.children.length; index += 1) {
+    const randomNumber = Math.floor(Math.random() * 3);
+    generatedLetter.children[index].classList.add(styleGroup[randomNumber]);
+    generatedLetter.children[index].classList.add(sizeGroup[randomNumber]);
+  }
+}
+
+function decorateRotationInclination() {
+  const randomNumber = Math.floor(Math.random() * 2);
+  for (let index = 0; index < generatedLetter.children.length; index += 1) {
+    const randomNumber = Math.floor(Math.random() * 2);
+    generatedLetter.children[index].classList.add(rotationGroup[randomNumber]);
+    generatedLetter.children[index].classList.add(inclinationGroup[randomNumber]);
   }
 }
 
