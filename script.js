0b827a5f66832da07d2850e813b7da9923dd3c2b
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-07-12 15:24:55 -0300

    Requisito número 3

---
+++ script.js
@@ -0,0 +1,20 @@
+const letterInput = document.getElementById('carta-texto');
+const generatedLetter = document.getElementById('carta-gerada');
+const letterGeneratorBtn = document.getElementById('criar-carta');
+let typedText = '';
+
+function getInput(event) {
+  typedText = event.target.value;
+}
+
+function createSpan() {
+  const stringArr = typedText.split(" ");
+  for (let index = 0; index < stringArr.length; index += 1) {
+    let createdSpan = document.createElement('span');
+    createdSpan.innerText = stringArr[index];
+    generatedLetter.appendChild(createdSpan);
+  }
+}
+
+letterGeneratorBtn.addEventListener('click', createSpan);
+letterInput.addEventListener('keyup', getInput);
