f5d0d5c280f002f14338b43ef13ae3db4fc07ad4
dhenycruz <dheniarley.ds@gmail.com>
2021-07-13 12:55:19 -0300

    Requisito #5.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // script
 function generatorLetter() {
   const letter = document.querySelector('#carta-texto').value;
+  if (letter === 0 || !letter.trim()) {
+    document.querySelector('#carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.';
+  }
   const arrayLetter = letter.split(' ');
   const lettesPrevious = document.querySelectorAll('span');
   lettesPrevious.forEach((spans) => {
