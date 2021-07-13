a5464c0465537f45bd4713541b99ec4c6d01d360
dhenycruz <dheniarley.ds@gmail.com>
2021-07-13 01:02:10 -0300

    Requisitos #3 e #4.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // script
+function generatorLetter() {
+  const letter = document.querySelector('#carta-texto').value;
+  const arrayLetter = letter.split(' ');
+  const lettesPrevious = document.querySelectorAll('span');
+  lettesPrevious.forEach((spans) => {
+    spans.parentElement.removeChild(spans);
+  });
+  arrayLetter.forEach((word) => {
+    const spanLetter = document.createElement('span');
+    spanLetter.innerText = word;
+    document.querySelector('#carta-gerada').appendChild(spanLetter);
+  });
+}
+
+const buttonGeneratorLetter = document.querySelector('#criar-carta');
+
+buttonGeneratorLetter.addEventListener('click', generatorLetter);
