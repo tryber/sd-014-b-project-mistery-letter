ae4665a97b649aa96dedfe5d726c6250c2fca189
dhenycruz <dheniarley.ds@gmail.com>
2021-07-13 13:44:10 -0300

    Corrigindo erro requisito #6.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // script
 function generatorLetter() {
   const letter = document.querySelector('#carta-texto').value;
+  /**
+   * Source https://pt.stackoverflow.com/questions/58498/se-input-tiver-apenas-espa%C3%A7os-em-branco-n%C3%A3o-fazer-nada
+   * Para verificar se o value de um input está vazio ou com vários espaços em branco.
+   */
   if (letter === 0 || !letter.trim()) {
     document.querySelector('#carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.';
   }
