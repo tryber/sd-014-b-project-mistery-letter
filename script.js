1a8956b0286b5e0242e33071bd559850b8ea52ad
Guilherme Neri <guinsneri@gmail.com>
2021-07-13 13:52:46 -0300

    Desafio 5

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 function addLetter() {
   const delLetter = document.getElementById('carta-gerada');
-  while (delLetter.firstChild) { delLetter.removeChild(delLetter.firstChild); }
-  const letter = document.getElementById('carta-texto').value.split(' ');
-  letter.forEach((element) => {
-    const span = document.createElement('span');
-    span.innerText = element;
-    const p = document.getElementById('carta-gerada');
+  const p = document.getElementById('carta-gerada');
+  let span = document.createElement('span');
+  if (document.getElementById('carta-texto').value.trim() === '') {
+    span = document.createElement('span');
+    span.innerText = 'Por favor, digite o conteúdo da carta.';
     p.appendChild(span);
-  });
+  } else {
+    while (delLetter.firstChild) { delLetter.removeChild(delLetter.firstChild); }
+    const letter = document.getElementById('carta-texto').value.split(' ');
+    letter.forEach((element) => {
+      span = document.createElement('span');
+      span.innerText = element;
+      p.appendChild(span);
+    });
+  }
 }
 const createLetter = document.getElementById('criar-carta');
 createLetter.addEventListener('click', addLetter);
