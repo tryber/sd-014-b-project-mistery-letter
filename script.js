89500f8e94b25c32b2d043cbdd9c7ce89eb2ab60
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-07-14 11:16:57 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -33,6 +33,15 @@ function criarCarta() {
       paragrafo.innerHTML += palavras[i];
     }
 
+    document.getElementById('carta-contador').innerHTML = palavras.length;
+
+    let botaoSpans = document.querySelectorAll('span');
+    for (let i = 0; i < botaoSpans.length; i += 1) {
+      botaoSpans[i].addEventListener('click', mudarEstilo);
+    }
+    
+    gerarClassAleatoria(document.querySelectorAll('span'));
+
   } else {
 
     let paragrafo = document.getElementById('carta-gerada');
@@ -40,7 +49,73 @@ function criarCarta() {
 
   }
 
-  console.log(document.getElementById('carta-gerada'));
+}
+
+function mudarEstilo(event) {
+  
+  let classEstilo = ['newspaper', 'magazine1', 'magazine2'];
+  let classTamanho = ['medium', 'big', 'reallybig'];
+  let classRotacao = ['rotateleft', 'rotateright'];
+  let classInclinacao = ['skewleft', 'skewright'];
+
+  let numberAleatorio = parseInt(Math.random() * 3) + 2;
+
+  event.target.className = '';
+
+  if (numberAleatorio === 2) {
+    
+      event.target.classList.add(classEstilo[parseInt(Math.random() * 3)]);
+      event.target.classList.add(classTamanho[parseInt(Math.random() * 3)]);
+    
+  } else if (numberAleatorio === 3) {
+
+    event.target.classList.add(classEstilo[parseInt(Math.random() * 3)]);
+    event.target.classList.add(classTamanho[parseInt(Math.random() * 3)]);
+    event.target.classList.add(classRotacao[parseInt(Math.random() * 2)]);
+
+  } else {
+
+    event.target.classList.add(classEstilo[parseInt(Math.random() * 3)]);
+    event.target.classList.add(classTamanho[parseInt(Math.random() * 3)]);
+    event.target.classList.add(classRotacao[parseInt(Math.random() * 2)]);
+    event.target.classList.add(classInclinacao[parseInt(Math.random() * 2)]);
+
+  }
+
+}
+
+function gerarClassAleatoria(spans) {
+
+  let classEstilo = ['newspaper', 'magazine1', 'magazine2'];
+  let classTamanho = ['medium', 'big', 'reallybig'];
+  let classRotacao = ['rotateleft', 'rotateright'];
+  let classInclinacao = ['skewleft', 'skewright'];
+
+  for (let i = 0; i < spans.length; i += 1) {
+
+    let numberAleatorio = parseInt(Math.random() * 3) + 2;
+
+    if (numberAleatorio === 2) {
+    
+      spans[i].classList.add(classEstilo[parseInt(Math.random() * 3)]);
+      spans[i].classList.add(classTamanho[parseInt(Math.random() * 3)]);
+    
+    } else if (numberAleatorio === 3) {
+
+      spans[i].classList.add(classEstilo[parseInt(Math.random() * 3)]);
+      spans[i].classList.add(classTamanho[parseInt(Math.random() * 3)]);
+      spans[i].classList.add(classRotacao[parseInt(Math.random() * 2)]);
+
+    } else {
+
+      spans[i].classList.add(classEstilo[parseInt(Math.random() * 3)]);
+      spans[i].classList.add(classTamanho[parseInt(Math.random() * 3)]);
+      spans[i].classList.add(classRotacao[parseInt(Math.random() * 2)]);
+      spans[i].classList.add(classInclinacao[parseInt(Math.random() * 2)]);
+
+    }
+    
+  }
 
 }
 
