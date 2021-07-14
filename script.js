15616bc1abd2c147f77533076b130530488b4b8a
Juliana Braga <juliana4braga@gmail.com>
2021-07-14 17:31:53 -0300

    Ajustes

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ let button = document.querySelector("#criar-carta");
 let input = document.querySelector("#carta-texto");
 
 function erro() {
-  let pararafo = document.querySelector("#carta-gerada");
+  let paragrafo = document.querySelector("#carta-gerada");
   let valorRecebido = input.value;
   if (valorRecebido == '' || valorRecebido.trim() == '') {
-    pararafo.innerText = 'Por favor, digite o conteúdo da carta.'
+    paragrafo.innerText = 'Por favor, digite o conteúdo da carta.'
   }
 }
 
 button.addEventListener("click", erro);
+
+function criarCarta() {
+  let valorRecebido = input.value;
+  let palavras = valorRecebido.split(" ");
+  console.log(palavras);
+  for (let palavra of palavras) {
+    let span = document.createElement("span");
+    span.innerHTML = palavra;
+    document.querySelector("#carta-gerada").appendChild(span);
+  }
+}
+
+button.addEventListener("click", criarCarta);
