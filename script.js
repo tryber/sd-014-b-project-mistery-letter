c803b1168130f2cc69c72cf58f4f29fd03ce9840
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2021-07-15 18:52:05 -0300

    ajustando linter

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function generate() {
   const arr = textInput.value.split(' ');
   counter.innerText = arr.length;
   function spanCreator() {
-    document.querySelector('#carta-gerada').innerHTML = '';
+    generatedText.innerHTML = '';
     for (let index = 0; index <= arr.length - 1; index += 1) {
       const lineSpan = document.createElement('span');
-      document.querySelector('#carta-gerada').appendChild(lineSpan);
+      generatedText.appendChild(lineSpan);
       lineSpan.innerText = arr[index];
     }
   }
