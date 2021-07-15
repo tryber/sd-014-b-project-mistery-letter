05229daa99d9f463bf5427a011096961afd60107
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2021-07-15 18:24:36 -0300

    Project Mistery Letter - requisito 18

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+// função para caso o input seja vazio
 const generateButton = document.querySelector('#criar-carta');
 const textInput = document.querySelector('#carta-texto');
+const generatedText = document.querySelector('#carta-gerada');
+const counter = document.querySelector('#carta-contador');
 
 function generate() {
-  console.log(textInput.value);
-  if (textInput.value === ' ' || textInput.value === '') {
-    document.querySelector('#carta-gerada').innerText = 'Por favor, digite o conteúdo da carta.';
+  if (textInput.value[0] === undefined || textInput.value[0] === ' ') {
+    generatedText.innerText = 'Por favor, digite o conteúdo da carta.';
   }
+  const arr = textInput.value.split(' ');
+  counter.innerText = arr.length;
 }
 
 generateButton.addEventListener('click', generate);
+
+// função para receber e tratar o input separando em uma array
