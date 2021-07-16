0cf8727d8ae8de6250b0a6fd126d2b1025b99a8b
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-07-16 09:40:19 -0400

    Requisitos 3 e 5

--- script.js
+++ script.js
@@ -2,16 +2,27 @@ const cartaTexto = document.querySelector("#carta-texto");
 const cartaGerada = document.querySelector("#carta-gerada");
 const criarCarta = document.querySelector("#criar-carta");
 
+function getLetterText() {
+  let getText = cartaTexto.value;
+  let getTextArray = getText.split(" ");
+  let paragrafo = document.querySelector("#carta-gerada")
+  paragrafo.innerHTML = '';
+  for (let index of getTextArray) {
+    let spanText = document.createElement('span');
+    spanText.innerHTML = index;
+    cartaGerada.appendChild(spanText);
+  }
+}
+
+criarCarta.addEventListener('click', getLetterText);
+
 // Fonte trim(): https://www.w3schools.com/jsref/jsref_trim_string.asp /
 function lookEmptySpaces() {
   let getText = cartaTexto.value;
   let emptyLetter = "Por favor, digite o conteúdo da carta.";
-  if (getText === '') {
-    getText.trim();
+  if (getText === '' || getText.trim() === '') {
     cartaGerada.innerText = emptyLetter;
-  } else {
-    cartaGerada.innerText = getText;
   }
 }
 
-criarCarta.addEventListener('click', lookEmptySpaces);
+criarCarta.addEventListener('click', lookEmptySpaces)
