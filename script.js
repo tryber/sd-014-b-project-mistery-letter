3ae88571d329090d8d18e8f30ce333fa5949c49c
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-07-17 20:25:38 -0300

    Requisito Bonus

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let input = document.getElementById('carta-texto');
 let btnCriarCarta = document.getElementById('criar-carta');
 let p = document.getElementById('carta-gerada');
+let pContador = document.getElementById('contador');
+let carta = document.getElementById('carta');
 
 // Limpa os campos span para gerar uma nova frase
 function limparCarta () {
   p.innerHTML = '';
+  pContador.innerHTML = '';
   inputVazio();
 }
 
@@ -12,6 +15,7 @@ function limparCarta () {
 function inputVazio() {
   let frase = input.value;
   frase = frase.trim();
+  input.value = frase;
   if (frase === '') {
     let addWord = document.createElement('span');
     addWord.innerHTML = 'Por favor, digite o conteúdo da carta.';
@@ -43,8 +47,17 @@ function gerarCarta() {
   addWord.id = 'span';
   p.appendChild(addWord);
   addClass();
+  contadorPalavras();
 }
 
+// Contador de palavras
+function contadorPalavras() {
+  let span = document.querySelectorAll('#span');
+  let contador = span.length;
+  pContador = document.createElement('p');
+  pContador.innerHTML = contador;
+  carta.appendChild(pContador);
+}
 // Adiciona classe as palavras
 function addClass() {
   let span = document.querySelectorAll('#span');
