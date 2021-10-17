a771447cdc419ac7de7196a67ed7409672f08db6
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-10-17 19:05:32 -0300

    requisitos 12, 13, 14 e 15

--- script.js
+++ script.js
@@ -2,27 +2,21 @@ const buttonCarta = document.getElementById('#criar-carta');
 const inputText = document.getElementById('#carta-texto').value;
 const cartaGerada = document.querySelector('#carta-gerada, span');
 const contador = document.querySelector('#carta-contador');
-const style = ['newspaper', 'magazine1', 'magazine2'];
-const size = ['medium', 'big', 'reallybig'];
-const rotate = ['rotateleft', 'rotateright'];
-const skew = ['skewleft', 'skewright'];
+const style = ['newspaper', 'magazine1', 'magazine2', 'skewleft', 'skewright'];
+const size = ['medium', 'big', 'reallybig', 'rotateleft', 'rotateright'];
 
 function geradordeCarta() {
   cartaGerada.innerText = '';
   const splInput = inputText.split(' ');
   contador.innerText = splInput.lenght;
-  if (splInput[0] === '') {
-    cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
-  } else {
-    for (let i = 0; i < splInput.length; i += 1) {
-      const span = document.createElement('span');
-      cartaGerada.appendChild(span);
-      span.innerText = splInput[i];
-      span.classList.add(style[Math.floor(Math.random() * 3)]);
-      span.classList.add(size[Math.floor(Math.random() * 3)]);
-      span.classList.add(rotate[Math.floor(Math.random() * 2)]);
-      span.classList.add(skew[Math.floor(Math.random() * 2)]);
-    }
+  cartaGerada.value = 'Por favor, digite o conteúdo da carta.';
+  for (let i = 0; i < splInput.length; i += 1) {
+    const span = document.createElement('div');
+    cartaGerada.appendChild(span);
+    span.innerText = splInput[i];
+    span.classList.createElement(style[Math.floor(Math.random() * 3)]);
+    span.classList.createElement(size[Math.floor(Math.random() * 3)]);
   }
 }
+
 buttonCarta.addEventListener('click', geradordeCarta);
